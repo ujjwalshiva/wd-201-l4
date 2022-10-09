995adb5b1bdbf6281deaa514c5c358b0eaaae3e2
Ujjwal Acharya <81429137+ujjwalshiva@users.noreply.github.com>
2022-10-09 16:16:35 +0530

    Updated toDisplayableList Function

--- todo.js
+++ todo.js
@@ -24,6 +24,35 @@ const todoList = () => {
       (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
     );
   };
+  
+  const toDisplayableList = (list) => {
+    var i;
+    var displayList = []
+    for (i=0;i<list.length;i++) {
+      if (list[i].completed === false) {
+        //`[ ] ${i.title} ${i.dueDate}`
+        if (list[i].dueDate===today) {
+          displayList.push(`[ ] ${list[i].title}`)
+        }
+        else {
+          displayList.push(`[ ] ${list[i].title} ${list[i].dueDate}`)
+        }
+      }
+      else {
+        // `[x] ${i.title} ${i.dueDate}`
+        if (list[i].dueDate===today) {
+          displayList.push(`[x] ${list[i].title}`)
+        }
+        else {
+          displayList.push(`[x] ${list[i].title} ${list[i].dueDate}`)
+        }          
+      }
+    }
+    
+    return displayList.join("\n");
+  }
+
+  
   return { all, add, markAsComplete, overdue, dueToday, dueLater };
 };
 
